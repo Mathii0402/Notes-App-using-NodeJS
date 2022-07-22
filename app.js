7d4cd164df06f7c1fd42d4255198bb6656b7d857
Mathii0402 <mathivananmv.cs20@bitsathy.ac.in>
2022-07-22 21:26:07 +0530

    notes app included with more commands

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ yargs.command(
                 type:"string"
         }
     },
-    handler:function(argv)
+    handler(argv)
     {
         return notes.addnotes(argv.title,argv.body)
     }
@@ -37,10 +37,68 @@ yargs.command(
         demandOption:true,
         type:'string'
     }},
-    handler:function(argv)
+    handler(argv)
     {
         return notes.removenotes(argv.title)
     }
 }
 )
+
+yargs.command(
+{
+    command:"list",
+    describe:"to list all notes",
+    handler()
+    {
+        return notes.listnotes()
+    }
+
+})
+
+yargs.command
+({
+    command:"read",
+    describe:"to read all the contents of the note",
+    builder:
+    {
+        title:
+        {
+            describe:"title of the note",
+            demandOption:true,
+            type:'string'
+        }},
+        handler(argv)
+        {
+            return notes.readnotes(argv.title)
+        }
+    
+})
+
+yargs.command
+({
+    command:"edit",
+    describe:"enter the title to edit",
+    builder:
+    {
+        title:
+        {
+            describe:"title",
+            demandOption:true,
+            type:"string"
+        },
+        body:
+        {
+                describe:"body",
+                demandOption:true, 
+                type:"string"
+        }
+   
+    },
+    handler(argv)
+    {
+        return notes.editnotes(argv.title,argv.body)
+    }
+
+})
+
 yargs.parse()
